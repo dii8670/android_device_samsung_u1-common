a46484b19a8bb4c589308f45d09afbf04604d82e
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-08-26 18:34:27 +0800

    u1-common: Update custom BT config board flag
     * AOSP now has its own implementation of custom configurations in N.
    
    Change-Id: I9cccd05bdeb7c313889eeefecfe0790b830b7c41

--- BoardCommonConfig.mk
+++ BoardCommonConfig.mk
@@ -146,7 +146,7 @@ WIFI_DRIVER_FW_PATH_P2P     := "/system/etc/wifi/bcmdhd_p2p.bin"
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := true
 BOARD_HAVE_SAMSUNG_BLUETOOTH := true
-BOARD_BLUEDROID_VENDOR_CONF := device/samsung/u1-common/bluetooth/vnd_smdk4210.txt
+BOARD_CUSTOM_BT_CONFIG := device/samsung/u1-common/bluetooth/vnd_smdk4210.txt
 
 # Selinux
 ifneq ($(filter u1ktt u1skt,$(TARGET_DEVICE)),)
